0e865824bda13de3aa95fce5e371077c339b08b4
maximtrp <maximtrp@gmail.com>
2019-07-26 01:23:16 +0400

    new ver

--- getData-gui-non-precise.py
+++ getData-gui-non-precise.py
@@ -16,7 +16,7 @@ import seaborn as sb
 filename_date = dt.datetime.now().strftime("%Y-%m-%d %H-%M-%S ECG.txt")
 filename_peaks = dt.datetime.now().strftime("%Y-%m-%d %H-%M-%S peaks.txt")
 
-delay_time = 40.
+delay_time = 10.
 data = ''
 
 sb.set(font_scale=1.5)
@@ -24,7 +24,8 @@ fig, ax = plt.subplots()
 
 line, = ax.plot(0, 0, 'k-', lw=1)
 plt.ylim(0,1000)
-plt.xlim(0,1000)
+xlim = 40000/delay_time
+plt.xlim(0,xlim)
 
 srl = serial.Serial("/dev/ttyUSB0", baudrate = 115200, timeout = 0)
 time.sleep(2)
@@ -39,23 +40,24 @@ def init():  # only required for blitting to give a clean slate.
 
 def animate(i):
     global data
-    data += srl.read(7).decode('cp1251')
+    data += srl.readline().decode('cp1251')
     y = list(map(int, data.split('\r\n')[:-1]))
     x = range(len(y))
 
     line.set_xdata(x)
     line.set_ydata(y)
 
-    ax.set_xlim(len(x) // 1000 * 1000, ((len(x)//1000)+1) * 1000)
+    ax.set_xlim(len(x) // xlim * xlim, ((len(x)//xlim)+1) * xlim)
     return line,
 
-ani = animation.FuncAnimation(fig, animate, init_func=init, interval=40, blit=True, frames=7500, repeat=False)
+ani = animation.FuncAnimation(fig, animate, init_func=init, interval=delay_time, blit=True, frames=int(300000/delay_time), repeat=False)
 plt.show()
 
 srl.write(b'0')
 srl.close()
 y = np.array(line.get_ydata(), dtype = np.int)
-x_peaks = pu.indexes(y, min_dist=10)
+x_peaks = pu.indexes(y, min_dist=int(350/delay_time), thres_abs=True, thres=(np.median(y) + np.diff(np.quantile(y, [0.25, 0.75]))[0] * 1.5))
+#x_peaks = pu.indexes(y, min_dist=int(350/delay_time))
 y_peaks = y[x_peaks]
 heart_rate = round(x_peaks.size * 60 / (len(y) * delay_time / 1000), 0)
 
@@ -63,3 +65,4 @@ x = np.arange(y.size) * delay_time
 np.savetxt(filename_date, np.stack((x, y)).T)
 np.savetxt(filename_peaks, np.stack((x_peaks * delay_time, y_peaks)).T)
 print("Heart rate is", heart_rate)
+
--- heartSensor_nogui.ino
+++ heartSensor_nogui.ino
@@ -1,25 +1,16 @@
-#include <U8g2lib.h>
-
 #define PIN 0
-#define DT 10.0
+#define DT 1
 #define LED 13
 #define CYCLELIM 350
 #define THRES 550
 
 boolean gap = false;
-unsigned char heartBeats = 0;
 unsigned char serialInt = 0;
 unsigned short rawPeak = 0;
-unsigned short cycles = 0;
-float heartRate = 0;
-
-U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0);
 
 void setup() {
   Serial.begin(115200);
   pinMode(LED, OUTPUT);
-  u8g2.begin();
-  u8g2.setFont(u8g2_font_logisoso62_tn);
 }
 
 void process(int value) {
@@ -29,8 +20,6 @@ void process(int value) {
   else if (value >= THRES && value < rawPeak) {
     if (!gap) {
       digitalWrite(LED, HIGH);
-      heartBeats++;
-      heartRate = heartBeats * 60.0 / (cycles * DT / 1000.0);
       gap = true;
     }
   }
@@ -41,22 +30,12 @@ void process(int value) {
   }
 }
 
-void outputRate() {
-  char HeartRateStr[3];
-  sprintf(HeartRateStr, "%d", (unsigned short)(floor(heartRate)));
-  unsigned short strWidth = u8g2.getStrWidth(HeartRateStr);
-  unsigned char height = 62;
-  u8g2.clearBuffer();
-  u8g2.drawStr((128 - strWidth) / 2, height, HeartRateStr);
-  u8g2.sendBuffer();
-}
 
 void loop() {
 
   /* Receive data from analog sensor */
   unsigned short rawValue = analogRead(PIN);
   process(rawValue);
-  cycles++;
 
   /* Send data using serial port */
   if (Serial.available() > 0) {
@@ -64,12 +43,6 @@ void loop() {
   }
   if (serialInt == 49) Serial.println(rawValue); 
 
-  if (cycles % 70 == 0) outputRate();
-
-  /* Reset settings on cycles end */
-  if (cycles > CYCLELIM) {
-    cycles = 1, heartBeats = 0, heartRate = 0; 
-  }
 
   delay(DT);
 }
